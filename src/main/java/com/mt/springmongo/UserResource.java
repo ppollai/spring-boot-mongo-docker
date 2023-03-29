16d7845ecbbd0410c926e176214728d48a4a169c
ppollai <prashantpollai@gmail.com>
2023-03-29 16:09:52 +0530

    New Project

--- src/main/java/com/mt/springmongo/UserResource.java
+++ src/main/java/com/mt/springmongo/UserResource.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Rest controller which is responsible for retrieving {@link User}s.
  *
- * @author Mithun
+ * @author ppollai
  */
 @RestController
 @RequestMapping(value = "/api/users")
